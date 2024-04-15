1e58c34efc440489c0e8a9b9ea23a7fe32b71eab
Stella Ndjikessi <stelladiana@live.fr>
2024-04-15 14:31:51 +0200

    Logo, name and basic navbar changed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './components css/Navbar.css';
+
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
+      <img src="SocNetLogo.png" alt="Logo"></img>
       <Link to="/">
-        <h1>Home</h1>
+        <h1>SocNet</h1>
       </Link>
+      <div></div>
     </div>
   )
 }
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -2,10 +2,13 @@ import './components css/Post.css';
 
 const Post = ({post}) => {
   return (
-    <div className="post">
-      <p>{post.createdAt}</p>
+    <div className="post-card">
+      <p className="post-date">{post.createdAt}</p>
       <p>{post.content}</p>
-      <p>Likes: {post.likes} Dislikes {post.dislikes}</p>
+      <div className="post-counters">
+        <p>Likes: <span>{post.likes}</span></p>
+        <p>Dislikes: <span>{post.dislikes}</span></p>
+      </div>
     </div>
   )
 }
